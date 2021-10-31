88f2bdbaea0d048a788c8b865458f790be97f112
ysBach <ysbach93@gmail.com>
2021-10-31 17:31:38 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,125 @@
-# amateur_astro_cat
-Find which amateur astronomy targets are above the horizon at given time, showing the altitude/airmass plot.
+# find_amateur_astro_target
+Yoonsoo P. Bach (@ysBach)
+
+This work is derived from the "서울대학교 천문대 공개행사", a subproject of "서울대학교 연구성과사회환원 프로그램" at Seoul National University, S. Korea.
+
+
+
+Find which **amateur astronomy targets** that are above the horizon at given time, and showing the altitude/airmass plot.
+
+Catalogs supported at this moment are [Messier](https://en.wikipedia.org/wiki/Messier_object) and [Caldwell](https://en.wikipedia.org/wiki/Caldwell_catalogue).
+
+
+
+Default location at Seoul, South Korea.
+
+
+
+## Dependency
+
+* python 3.6+
+* pytz
+* numpy
+* pandas
+* astropy
+* **astroplan**
+* matplotlib
+* rich (optional)
+
+
+
+### Install at once
+
+First, install Anaconda distribution. Then all may have been installed, except for `astroplan`. Install it by:
+
+```
+$ conda install -c astropy astroplan
+```
+
+
+
+Package `rich` is used for nothing but nicer output on terminal. You may install by `pip install rich`, but it is **not necessary**.
+
+
+
+## Usage
+
+When you use this for the ***first time***, it will take some time, like ***~ 5 minutes*** (as it tries to query the RA/DEC information of the objects from `astroquery`)
+
+```
+usage: find_targets.py [-h] [-U] [-M] [-C] [-X] [-A] [-d DURATION] [-l LOCATION LOCATION] [-z TIMEZONE] [-c] [-v] [-a MIN_ALT] [-p MIN_ALT_PL] [-t TARGETS [TARGETS ...]] [-o OUTPUT]
+                       [YYYY] [MM] [DD] [HH] [mm] [ss]
+
+positional arguments:
+  YYYY                  Year; If not given, current time is used
+  MM                    Month (1 <= month <= 12)
+  DD                    Date (1 <= day <= number of days in the given month and year)
+  HH                    Hour (0 <= hour < 24)
+  mm                    Minutes (0 <= minute < 60)
+  ss                    Seconds (0 <= second < 60)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -U, --UTC             Time is in UTC instead of local time
+  -M, --Messier         Do NOT use Messier objects
+  -C, --Caldwell        Do NOT use Caldwell objects
+  -X, --airmass         Whether to use the primary y-axis as airmass (`X`)
+  -A, --always-visible  Whether to discard objects that are not *always* visible during the timespan
+  -d DURATION, --duration DURATION
+                        Duration of the observing run (+- this number) [hour]
+  -l LOCATION LOCATION, --location LOCATION LOCATION
+                        (longitude, latitude) in degrees; Used in astropy.coordinates.EarthLocation
+  -z TIMEZONE, --timezone TIMEZONE
+                        Time zone
+  -c, --currentlocation
+                        Use current location and timezone automatically (using http://ip-api.com/json/)
+  -v, --verbose         Print miscellaneous information
+  -a MIN_ALT, --min-alt MIN_ALT
+                        Minimum altitude of the object to be drawn [deg]
+  -p MIN_ALT_PL, --min-alt-pl MIN_ALT_PL
+                        Minimum altitude of the planetary objects to be drawn [deg]
+  -t TARGETS [TARGETS ...], --targets TARGETS [TARGETS ...]
+  -o OUTPUT, --output OUTPUT
+                        HTML name to save DataFrame
+```
+
+
+
+### Example
+
+First, check for ALL the objects above the horizon on the day. I will test with 
+
+* 2021-11-12 20:30:00 [KST] (given by``2021 11 12 20 30``) 
+* duration of ± 1 hour (given by `-d 1`)
+* do ***NOT*** plot Caldwell catalog (use `-C`)
+* save all target list as ``test.html`` (`-o test.html`)
+* use verbose option (`-v`)
+
+```
+$ python find_targets.py 2021 11 12 20 30 00 -o test.html -d 1 -v -C
+```
+
+This is messy, but it is intended.
+
+![](examples/Figure_1.png)
+
+* **NOTE**: line style is grouped based on the type of the objects (cluster, neubla, galaxy, otehrs)
+* **NOTE**: For the Moon, ``θ_full`` means the 180˚ - (Sun-Earth-Moon angle), i.e., the angle for the Moon to travel to become a full moon.
+
+The output HTML looks like this:
+
+![](examples/Figure_2.png)
+
+Then, considering the time limit and the interest of the audience (e.g., depending on the topic of the lecture given before the observation), **let's select few targets**. 
+
+On 2021-11-12, we have a lecture on Sombrero galaxy (M104), but it is not visible, unfortunately. Similar galaxies, S/E types, can be chosen from the list above. I will select M31, M32, M110. Also, let me choose some globular clusters that are attractive to general audience, M72, and an open cluster M45.
+
+* Select targets by `-t M31 M32 M110 M72 M45`
+
+```
+$ python find_targets.py 2021 11 12 20 30 00 -o test.html -d 1 -v -C -t M31 M32 M110 M72 M45
+```
+
+The plot looks simpler, but you have to note that M72 and M45 can be below altitude 30˚ (the lower limit for SNU-SAO 1m telescope) during the ± 1hour duration.
+
+![](examples/Figure_3.png)
---
+++ find_targets.py
@@ -0,0 +1,306 @@
+# %%
+import argparse
+import datetime
+from pathlib import Path
+
+import astroplan as ap
+import astropy.units as u
+import numpy as np
+import pandas as pd
+import pytz
+from astroplan import plots as aplt
+from astropy.coordinates import EarthLocation, get_body
+from astropy.time import Time
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+import warnings
+warnings.filterwarnings('ignore', append=True)
+
+try:
+    from rich import print
+    from rich.traceback import install
+    install()
+except ImportError:
+    print("You may want to install `rich` by $ pip install rich")
+    pass
+
+# We need to do it in a separate cell. See:
+# https://github.com/jupyter/notebook/issues/3385
+plt.style.use('default')
+rcParams.update({
+    'font.family': 'Times', 'font.size': 12, 'mathtext.fontset': 'stix',
+    'axes.formatter.use_mathtext': True, 'axes.formatter.limits': (-4, 4),
+    'axes.grid': True, 'grid.color': 'gray', 'grid.linewidth': 0.5,
+    'xtick.top': True, 'ytick.right': True,
+    'xtick.direction': 'inout', 'ytick.direction': 'inout',
+    'xtick.minor.size': 4.0, 'ytick.minor.size': 4.0,  # default 2.0
+    'xtick.major.size': 8.0, 'ytick.major.size': 8.0,  # default 3.5
+    'xtick.minor.visible': True, 'ytick.minor.visible': True
+})
+
+PLANETS = {
+    'Mercury': "gray", 'Venus': "brown", 'Mars': "red",
+    'Jupiter': "orange", 'Saturn': "olive", 'Uranus': "lightseagreen", 'Neptune': "blue"
+}
+
+LSDICT = {
+    'gal': "-", 'Neb': "--", "cl": "-."
+}
+
+INFOSTR = """
+Abbreviations for "Type" column
+* cl: Cluster
+  - cl-O: open cluster
+  - cl-G: globular cluster
+* gal: Galaxy
+  - gal-SB: Barred spiral galaxy
+  - gal-S: Spiral galaxy
+  - gal-S0: Lenticular galaxy
+  - gal-E: Elliptical galaxy
+  - gal-dE: Dwarf elliptical galaxy
+  - gal-dSph: Dwarf Spheroidal Galaxy
+  - gal-cD: Supergiant Elliptical Galaxy
+  - gal-Irr: Irregular galaxy
+  - gal-IrrB: Barred irregular galaxy
+  - gal-Inter: Interacting galaxy
+* Neb: Nebula
+  - Neb-P: Planetary nebula
+  - Neb-HII: H II Region Nebula
+  - Neb-SN: Supernova remnant
+* MW: Milky Way
+
+Original of Messier data from https://en.wikipedia.org/wiki/Messier_object
+Original of Caldwell data from https://en.wikipedia.org/wiki/Caldwell_catalogue
+"""
+
+p = argparse.ArgumentParser(description='')
+
+# TODO: let user choose which to use first y-axis (airmass, altitude)
+p.add_argument("YYYY", nargs='?',
+               help="Year; If not given, current time is used")
+p.add_argument("MM", nargs='?', default=1,
+               help="Month   (1 <= month <= 12)")
+p.add_argument("DD", nargs='?', default=1,
+               help="Date    (1 <= day <= number of days in the given month and year)")
+p.add_argument("HH", nargs='?', default=0,
+               help="Hour    (0 <= hour < 24)")
+p.add_argument("mm", nargs='?', default=0,
+               help="Minutes (0 <= minute < 60)")
+p.add_argument("ss", nargs='?', default=0,
+               help="Seconds (0 <= second < 60)")
+
+p.add_argument("-U", "--UTC", action="store_true",
+               help="Time is in UTC instead of local time")
+p.add_argument("-M", "--Messier", action="store_false",
+               help="Do NOT use Messier objects")
+p.add_argument("-C", "--Caldwell", action="store_false",
+               help="Do NOT use Caldwell objects")
+p.add_argument("-X", "--airmass", action="store_true",
+               help="Whether to use the primary y-axis as airmass (`X`)")
+p.add_argument("-A", "--always-visible", action="store_true",
+               help="Whether to discard objects that are not *always* visible during the timespan")
+
+p.add_argument("-d", "--duration", default=2., type=float,
+               help="Duration of the observing run (+- this number) [hour]")
+p.add_argument("-l", "--location", nargs=2, default=[127, 37.5],
+               help="(longitude, latitude) in degrees; Used in astropy.coordinates.EarthLocation")
+p.add_argument("-z", "--timezone", default="Asia/Seoul", type=str,
+               help="Time zone")
+p.add_argument("-c", "--currentlocation", action="store_true",
+               help="Use current location and timezone automatically (using http://ip-api.com/json/)")
+p.add_argument("-v", "--verbose", action="store_true",
+               help="Print miscellaneous information")
+
+p.add_argument("-a", "--min-alt", default=30., type=float,
+               help="Minimum altitude of the object to be drawn [deg]")
+p.add_argument("-p", "--min-alt-pl", default=25., type=float,
+               help="Minimum altitude of the planetary objects to be drawn [deg]")
+
+p.add_argument("-t", "--targets", nargs='+', default=None)
+p.add_argument("-o", "--output", default=None, help="HTML name to save DataFrame")
+
+args = p.parse_args()
+
+
+def check_observable(min_alt, observer, targets, times, always):
+    consts = [ap.AltitudeConstraint(min_alt*u.deg), ap.AtNightConstraint(max_solar_altitude=0*u.deg)]
+    mask_fun = ap.is_always_observable if always else ap.is_observable
+    mask = mask_fun(constraints=consts, observer=observer, targets=targets.tolist(), times=times)
+    # coo_up = []
+    # for _m, _t in zip(mask, targets):
+    #     if _m:
+    #         coo_up.append(_t)
+    return mask
+
+
+def get_geoloc(use_current_location, verbose):
+    ''' Find geological location information from ip-api.com (lon, lat, timezone).
+    '''
+    if use_current_location:
+        # TODO: Save these as cache files
+        from requests import get
+        ip = get('https://api.ipify.org').content.decode('utf8')
+        response = get("http://ip-api.com/json/" + ip).json()
+        # {'status': 'success', 'country': 'South Korea', 'countryCode': 'KR', 'region': '11',
+        # 'regionName': 'Seoul', 'city': 'Gwanak-gu', 'zip': '08841', 'lat': 37.4625, 'lon': 126.9438,
+        # 'timezone': 'Asia/Seoul', 'isp': 'SNU', 'org': '', 'as': 'AS9488 Seoul National University',
+        # 'query': '147.46.135.73'}
+        lon = float(response['lon'])*u.deg
+        lat = float(response['lat'])*u.deg
+        tz = pytz.timezone(response['timezone'])
+        if verbose:
+            print(response)
+    else:
+        lon = float(args.location[0])*u.deg
+        lat = float(args.location[1])*u.deg
+        tz = pytz.timezone(args.timezone)
+    return lon, lat, tz
+
+
+def get_time(YYYY, MM, DD, HH, mm, ss, in_utc):
+    ''' Parse the time infromation
+    '''
+    if YYYY is None:
+        _obstime = datetime.datetime.utcnow()
+    else:
+        YYYY = int(YYYY)
+        MM = int(MM)
+        DD = int(DD)
+        HH = int(HH)
+        mm = int(mm)
+        ss = int(ss)
+        if in_utc:
+            _obstime = datetime.datetime(YYYY, MM, DD, HH, mm, ss)
+        else:
+            _obstime = datetime.datetime(YYYY, MM, DD, HH, mm, ss).astimezone(tz)
+    return _obstime
+
+
+if __name__ == "__main__":
+    if args.verbose:
+        print(INFOSTR)
+        print(args)
+
+    # == Get location and time information ================================================================= #
+    lon, lat, tz = get_geoloc(args.currentlocation, args.verbose)
+    _obstime = get_time(args.YYYY, args.MM, args.DD, args.HH, args.mm, args.ss, args.UTC)
+
+    print(f"Date & Time : {_obstime} ({tz})\n lon , lat  : {lon.value:.2f}˚, {lat.value:.2f}˚")
+
+    OBSTIME = Time(_obstime)  # in UTC
+    dt = args.duration*u.hour
+    OBSTIME_RANGE = Time([OBSTIME - dt, OBSTIME + dt])
+    OBSTIMES = OBSTIME + np.linspace(-dt, dt, max(6, int(args.duration*12)))
+    # around once per 5 minutes
+
+    # NOTE: Default elevation of the observatory is set to 500m. Only small offset will be added,
+    #   and that is insignificant for the purpose (maybe about the size of line width...).
+    loc = EarthLocation.from_geodetic(lon, lat, 500*u.m)
+    obs = ap.Observer(location=loc, timezone=tz)
+
+    # == Prepare catalog =================================================================================== #
+    cat = pd.read_csv(Path(__file__).parent/"amastro_catalog.csv", delimiter='\t', comment="#")
+    if not args.Messier:
+        cat = cat[~cat["ID"].str.startswith("M")]
+    if not args.Caldwell:
+        cat = cat[~cat["ID"].str.startswith("C")]
+    if args.targets is not None:
+        cat = cat[cat["ID"].isin(args.targets)]
+
+    cat.sort_values(by="Type", inplace=True)
+
+    # == Find astrometric information of targets (one time only) =========================================== #
+    coo = []
+    # Once it is done, the info will be cached to the astropy cache directory.
+    # You may clean it by astropy.utils.clear_download_cache()
+    # See https://docs.astropy.org/en/stable/utils/index.html
+    for _, row in cat.iterrows():
+        catid = row['ID']
+        othid = row["Other ID"]
+        if not isinstance(othid, float):  # if it was a float, it means it was a NaN.
+            othid = othid.split(" & ")[0]
+
+        try:
+            _coo = ap.FixedTarget.from_name(catid)
+        except:  # whatever Error occurs...
+            _coo = ap.FixedTarget.from_name(othid)
+        _coo.name = catid + f"({row['Type']})"
+        coo.append(_coo)
+
+    coo = np.array(coo)
+    cat["RA"] = [_coo.ra.deg for _coo in coo]
+    cat["DEC"] = [_coo.dec.deg for _coo in coo]
+
+    # == Find coordinates of planets ======================================================================= #
+    coo_pl = []
+    for planet in PLANETS.keys():
+        _coo = get_body(planet, time=OBSTIME, location=loc)
+        _coo.name = planet
+        coo_pl.append(_coo)
+    # RA/DEC only at the middle of the time.
+    coo_pl = np.array(coo_pl)
+
+    # style_ini = dict(style_kwargs=dict(marker=".", color="r"))
+    # style_mid = dict(style_kwargs=dict(marker="x", color="r"))
+    # style_end = dict(style_kwargs=dict(marker="o", color="r"))
+    # OBSTIMES_5 = OBSTIME + np.linspace(-1, 1, 5)*u.hour
+    # aplt.plot_sky(target=cat["coord"][0], observer=obs, time=OBSTIMES_5[:-1], **style_ini)
+    # aplt.plot_sky(target=cat["coord"][0], observer=obs, time=OBSTIMES_5[2], **style_mid)
+    # aplt.plot_sky(target=cat["coord"][0], observer=obs, time=OBSTIMES_5[-1], **style_end)
+
+    # def find_coo_up(coords, generous=False):
+    #     times = OBSTIMES if generous else OBSTIME
+    #     horizon = 25*u.deg if generous else 30*u.deg
+    #     coo_up = [_coo for _coo in coords if np.any(observer.target_is_up(times, _coo, horizon=horizon))]
+    #     return coo_up
+
+    # == Plot ============================================================================================== #
+    colors = np.vstack([plt.cm.tab10(np.linspace(0, 1, 10)),
+                        plt.cm.tab20(np.linspace(0, 1, 20))[1::2]])
+    observable_kw = dict(observer=obs, times=OBSTIMES, always=args.always_visible)
+    upmask = check_observable(args.min_alt, targets=coo, **observable_kw)
+    coo_up = coo[upmask]
+    cat_up = cat[upmask]
+
+    cat_up
+    if args.output is not None:
+        cat_up.to_html(args.output, index=False)
+        if args.verbose:
+            print(f"Catalog saved to {args.output}")
+
+    lss = np.array([":"]*len(cat_up), dtype="<U2")
+    for k, v in LSDICT.items():
+        lss[cat_up["Type"].str.startswith(k)] = v
+
+    fig, axs = plt.subplots(1, 1, figsize=(9, 9), sharex=False, sharey=False, gridspec_kw=None)
+
+    for i, (_coo, ls) in enumerate(zip(coo_up, lss)):
+        aplt.plot_altitude(
+            targets=_coo, observer=obs, time=OBSTIMES, ax=axs, min_altitude=args.min_alt,
+            style_kwargs=dict(linestyle=ls, color=colors[i%10], alpha=0.7, linewidth=2)
+        )
+
+    moon_phase = obs.moon_phase(OBSTIME).to(u.deg).value
+    # in radian, phase=pi is “new”, phase=0 is “full”.
+    alt_moon = obs.moon_altaz(OBSTIMES).alt
+    axs.plot_date(OBSTIMES.plot_date, alt_moon, '-', color='k', linewidth=6, alpha=0.3,
+                  label=f'Moon (θ_full={moon_phase:.0f}˚)')
+
+    upmask_pl = check_observable(args.min_alt_pl, targets=coo_pl, **observable_kw)
+    coo_pl_up = coo_pl[upmask_pl]
+    for _coo in coo_pl_up:
+        aplt.plot_altitude(targets=_coo, observer=obs, time=OBSTIMES, ax=axs, min_altitude=args.min_alt,
+                           style_kwargs=dict(color=PLANETS[_coo.name], linewidth=6, alpha=0.3))
+
+    fake_coo = coo_pl_up[0].copy()
+    fake_coo.name = None
+    aplt.plot_altitude(targets=fake_coo, observer=obs, time=OBSTIMES, ax=axs,
+                       min_altitude=args.min_alt, airmass_yaxis=True,
+                       style_kwargs=dict(linestyle=''))
+
+    axs.axhline(30, color='k', linestyle='-')
+    axs.legend(ncol=4, bbox_to_anchor=(0.5, -0.15), loc=9,
+               prop={"family": "monospace", "size": 10},
+               title="<Catalog ID> (<Type>). Use `-v` for detailed explanation for Type.")
+    plt.tight_layout()
+    plt.show()
